e632d810b6a175ae0e4949f4fd554c5147eb3b2d
Sravan <sravan.kollapudi@gmail.com>
2025-11-14 13:20:26 -0500

    Trigger deployment with GitHub Actions Pages source

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-// TechnoRetail Digital Transformation Platform
+// TechnoRetail Digital Transformation - GitHub Pages Deploy
 const queryClient = new QueryClient();
 
 const App = () => (
